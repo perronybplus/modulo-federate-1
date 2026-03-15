aacf990ab8ca6c76f68f09cee5fe1440dd91c93b
= <=>
2026-03-15 20:11:21 -0300

    update module 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "module_federate_1",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom", "react-router-dom"],
+    }),
+  ],
+  server: {
+    port: 5174,
+    origin: "http://localhost:5174",
+  },
+  preview: {
+    port: 5174,
+  },
+  build: {
+    target: "chrome89",
+  },
+});
